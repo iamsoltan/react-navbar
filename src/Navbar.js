14c265d9652b16d980f2cfb41e91246dd310df2b
mohamed khalil <khemeri.mohamed.khalil.simplon@gmail.com>
2020-04-21 22:21:43 +0100

    navbar added

---
+++ src/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="header">
+      <div className="header-container">
+        <div className="fright">
+          <img src={logo} width=" 268px" height="33px" alt="logo_petit" />
+        </div>
+        <div className="hovering">
+          {" "}
+          <a href="/#">About us</a>
+        </div>
+        <div className="hovering">
+          <a href="/#">Career</a>
+        </div>
+        <div className="hovering dropdown-btn">
+          {" "}
+          <a href="/#">Bootcamps</a>
+          <ul className="submenu">
+            <li>
+              <a href="/#">Mearn Stack</a>
+            </li>
+            <li>
+              <a href="/#">Mean Stack</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
